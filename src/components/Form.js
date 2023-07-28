9f6ad48432f0240bb40e432508610e53ccbb35d0
kipmaiyo <danielkipmaiyo44@gmail.com>
2023-07-28 21:25:59 +0300

    Completed Assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,108 @@
+//import React, { useState } from "react";
+
+// function Form() {
+//   const [firstName, setFirstName] = useState("John");
+//   const [lastName, setLastName] = useState("Henry");
+
+//   function handleFirstNameChange(event) {
+//     setFirstName(event.target.value);
+//   }
+
+//   function handleLastNameChange(event) {
+//     setLastName(event.target.value);
+//   }
+
+//   return (
+//     <form>
+//       <input type="text" onChange={handleFirstNameChange} value={firstName} />
+//       <input type="text" onChange={handleLastNameChange} value={lastName} />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+//export default Form
+
+
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    admin: false,
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  const [tableData, setTableData] = useState([]); // State to store the form data in a table
+
+  function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
+
+    // use `checked` property of checkboxes instead of `value`
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  function handleSubmit(event) {
+    event.preventDefault();
+    setTableData([...tableData, formData]); // Add the form data to the tableData array
+    setFormData({ // Clear the form fields after submission
+      firstName: "",
+      lastName: "",
+      admin: false,
+    });
   }
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="firstName"
+          onChange={handleChange}
+          value={formData.firstName}
+          placeholder="First Name"
+        />
+        <input
+          type="text"
+          name="lastName"
+          onChange={handleChange}
+          value={formData.lastName}
+          placeholder="Last Name"
+        />
+        <input
+          type="checkbox"
+          name="admin"
+          onChange={handleChange}
+          checked={formData.admin}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Admin</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((data, index) => (
+            <tr key={index}>
+              <td>{data.firstName}</td>
+              <td>{data.lastName}</td>
+              <td>{data.admin ? "True" : "False"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
